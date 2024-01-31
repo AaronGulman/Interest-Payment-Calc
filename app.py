d2f24adb6f0ddafca874a8f3fabc748b07571033
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-30 22:36:59 -0500

    Display the result

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ def main():
         print("")
         
         principal = float(input("Enter the amount borrowed: "))
-        apr = input("Enter the annual interest rate: ")
-        years = int(input("Enter teh duration of the loan(in years): "))
+        apr = float(input("Enter the annual interest rate: "))
+        years = int(input("Enter the duration of the loan(in years): "))
         
         
         monthly_interest_rate = apr / 1200
         number_of_months = years * 12 
         monthly_payment = principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-number_of_months))
         
+        print("The monthly payment is: $" + str(monthly_payment,))
         
+main()
