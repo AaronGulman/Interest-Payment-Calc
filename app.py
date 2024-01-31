60dbb5919b4e3b172890b7e71b7a69c51498791b
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-30 22:35:38 -0500

    Add user input requests

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ def main():
         print("Welcome to the Interest Payment Calculator")
         print("")
         
+        principal = float(input("Enter the amount borrowed: "))
+        apr = input("Enter the annual interest rate: ")
+        years = int(input("Enter teh duration of the loan(in years): "))
+        
+        
