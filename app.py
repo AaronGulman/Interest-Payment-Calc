49ee41564e380b9d0814b4fd8c69cf05a6c84433
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-30 22:36:10 -0500

    Add payment calculations

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ def main():
         years = int(input("Enter teh duration of the loan(in years): "))
         
         
+        monthly_interest_rate = apr / 1200
+        number_of_months = years * 12 
+        monthly_payment = principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-number_of_months))
+        
+        
